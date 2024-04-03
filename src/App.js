556da9c5d10439d5f1855ba0ea876182cbb5f069
Antonio Papa <antoniopapa1991@gmail.com>
2024-04-03 17:10:42 +0200

    1-Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className="container">
+        <div className="row">
+            <div className="col-3">
+                <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary">
+                    <div className="list-group list-group-flush border-bottom scrollarea">
+                        <a href="#" className="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                            <div className="d-flex w-100 align-items-center justify-content-between">
+                                <strong className="mb-1">List group item heading</strong>
+                                <small>Wed</small>
+                            </div>
+                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                        </a>
+                        <a href="#" className="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                            <div className="d-flex w-100 align-items-center justify-content-between">
+                                <strong className="mb-1">List group item heading</strong>
+                                <small>Wed</small>
+                            </div>
+                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-9">
+
+            </div>
+        </div>
     </div>
-  );
 }
 
 export default App;
