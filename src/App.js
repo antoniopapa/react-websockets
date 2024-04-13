fc86cccb91af043ffe314b5cb7425e6db24c127a
Antonio Papa <antoniopapa1991@gmail.com>
2024-04-13 12:57:52 +0200

    3-Socket.io

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {socket} from "./index";
 
 function App() {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        socket.on('message', (msg) => {
+            setMessages(messages => [...messages, msg])
+        })
+
+        return () => {
+            socket.off('message')
+        }
+    }, []);
+
+    const submit = async (e) => {
+        e.preventDefault()
+
+        await axios.post('http://localhost:8000/api/message', {
+            message
+        })
+
+        setMessage('')
+    }
+
     return <div className="container">
         <div className="row">
             <div className="col-3 vh-100">
@@ -24,34 +50,37 @@ function App() {
                 </div>
 
                 <div id="conversation">
-                    <div className="row pt-2">
-                        <div className="col-6">
-                            <div className="alert d-inline-block alert-primary" role="alert">
-                                Hi
+                    {messages.map(m => {
+                        return <div className="row pt-2" key={m}>
+                            <div className="col-6">
+                                <div className="alert d-inline-block alert-primary" role="alert">
+                                    {m}
+                                </div>
+                            </div>
+
+                            <div className="col-6">
+
                             </div>
                         </div>
+                    })}
 
-                        <div className="col-6">
 
-                        </div>
-                    </div>
+                    {/*<div className="row pt-2">*/}
+                    {/*    <div className="col-6">*/}
 
-                    <div className="row pt-2">
-                        <div className="col-6">
+                    {/*    </div>*/}
 
-                        </div>
-
-                        <div className="col-6">
-                            <div className="alert d-inline-block alert-success float-end" role="alert">
-                                Hello there
-                            </div>
-                        </div>
-                    </div>
+                    {/*    <div className="col-6">*/}
+                    {/*        <div className="alert d-inline-block alert-success float-end" role="alert">*/}
+                    {/*            Hello there*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
 
-                <form id="reply" className="p-3 w-100">
+                <form id="reply" className="p-3 w-100" onSubmit={submit}>
                     <div className="input-group">
-                        <input className="form-control" placeholder="Write a message"/>
+                        <input className="form-control" placeholder="Write a message" onChange={e => setMessage(e.target.value)}/>
                     </div>
                 </form>
 
